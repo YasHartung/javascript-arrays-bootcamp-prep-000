974721379d945f479ff8888f79fd56f81e31f1ad
Yasmine Hartung <y.hartung@gmail.com>
2019-01-20 08:28:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,6 @@ function destructivelyAddElementToEndOfArray (array, element) {
   return array
 }
 function accessElementInArray (array, index){
-  console.log(array[index])
-  return
+   return array[index]
+
 }
